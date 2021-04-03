42aa4cf72ea925bb60c0282148b90eaba9a92bb0
Carsten Schmitt <carsten.schmitt@lost-infinity.com>
2021-04-03 10:51:15 +0200

    Test

--- source/focus_finder/gui/spline_curve_mapper_panel.cpp
+++ source/focus_finder/gui/spline_curve_mapper_panel.cpp
@@ -26,6 +26,7 @@
 
 #include "ui_spline_curve_mapper_panel.h"
 
+// TEST
 SplineCurveMapperPanelT::SplineCurveMapperPanelT(QWidget *parent, FocusFinderLogicT &ffl,
                                                  const std::string &functionMapperName, bool visible)
         : AbstractMapperPanelT(parent),
