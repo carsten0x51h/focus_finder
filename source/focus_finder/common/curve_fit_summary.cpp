ae7d2829f6ab4a7824549ec539beb7f950fde48e
Carsten Schmitt <carsten.schmitt@lost-infinity.com>
2022-03-12 23:58:44 +0100

    Added output of curve values.
    * Cleanup: Removed unnecessary global variables.
    * Cleanp: Removed commented code

--- source/focus_finder/common/curve_fit_summary.cpp
+++ source/focus_finder/common/curve_fit_summary.cpp
@@ -23,24 +23,22 @@
  ****************************************************************************/
 
 #include <ostream>
+#include <iomanip>
 
 #include "include/point_with_residual.h"
 #include "include/curve_fit_summary.h"
 
+CurveFitSummaryT::CurveFitSummaryT() : success(false), outlierBoundary(0.0F) {}
 
-bool success;                                // < Indicates if curve parameters were found under given constraints
-size_t numIterationsRequiredTotal = 0;        // < Number of required iterations in total
-float outlierBoundary;                        // < Contains outlier boundary in case outlier detection was enabled, otherwise -1
-std::vector<PointWithResidualT> outliers;    // < Contains outliers in case outlier detection was enabled
 
 std::ostream &operator<<(std::ostream &os, const CurveFitSummaryT &summary) {
     static const char *prefix = "   > ";
 
-    os << "** CurveFitSummary **" << std::endl;
+    os << "** CurveFitSummary **" << std::fixed << std::setprecision(20) << std::endl;
 
-    //success
+    // success
     os << prefix << "Curve fitting status" << std::endl;
-    os << prefix << (success ? "SUCCESS" : "FAILURE") << std::endl;
+    os << prefix << (summary.success ? "SUCCESS" : "FAILURE") << std::endl;
 
     // matchedDataPoints
     os << prefix << "Matched data points [#dp=" << summary.matchedDataPoints.size() << "]" << std::endl;
@@ -48,20 +46,22 @@ std::ostream &operator<<(std::ostream &os, const CurveFitSummaryT &summary) {
     for (const auto &p : summary.matchedDataPoints) {
         os << prefix << p << std::endl;
     }
+    os << std::endl;
 
-    // TODO: For some reason this does not compile..
-    // std::copy(summary.matchedDataPoints.begin(),
-    // 		summary.matchedDataPoints.end(),
-    // 	    std::ostream_iterator<PointFT>(os));
+    // curveDataPoints
+    os << prefix << "Curve data points [#dp=" << summary.curveDataPoints.size() << "]" << std::endl;
 
+    for (const auto &p : summary.curveDataPoints) {
+        os << prefix << p << std::endl;
+    }
     os << std::endl;
 
 
     // outliers
-    bool outlierDetectionEnabled = outlierBoundary >= 0;
+    bool outlierDetectionEnabled = (summary.outlierBoundary >= 0);
 
     if (outlierDetectionEnabled) {
-        os << prefix << "Outliers [#outliers=" << summary.outliers.size() << ", boundary=" << outlierBoundary << "]"
+        os << prefix << "Outliers [#outliers=" << summary.outliers.size() << ", boundary=" << summary.outlierBoundary << "]"
            << std::endl;
 
         for (const auto &pr : summary.outliers) {
@@ -77,8 +77,7 @@ std::ostream &operator<<(std::ostream &os, const CurveFitSummaryT &summary) {
     }
     os << std::endl;
 
-
-    os << prefix << "Number of iterations total=" << numIterationsRequiredTotal << std::endl;
+    os << prefix << "Number of iterations total=" << summary.numIterationsRequiredTotal << std::endl;
 
     return os;
 }
