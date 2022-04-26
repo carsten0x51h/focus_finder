4d9a5d07c559cf5211840e9ceed9ae93a9c4616d
Carsten Schmitt <carsten.schmitt@lost-infinity.com>
2022-04-26 07:09:39 +0200

    Added HFD test images.

---
+++ source/focus_finder/tests/starmath-tests/test_data/gaussian_normal_distribution_2d/generate_2d_gaussian_distribution.py
@@ -0,0 +1,37 @@
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+import math
+
+from libtiff import TIFF
+
+def gkern(kernlen=21, sigma=1, factor=65535):
+    """Returns a 2D Gaussian kernel array."""
+    gkern1d = signal.gaussian(kernlen, std=sigma).reshape(kernlen, 1)
+    gkern2d = factor*np.outer(gkern1d, gkern1d)
+    return gkern2d
+
+
+imageDimensionPx = 121
+
+
+imageDimensions = [50, 51, 100, 101]
+sigmaValues = [1, 2, 3, 4, 5]
+factorValues = ["norm", 32767, 65535]
+
+
+for dim in imageDimensions:
+    for sigma in sigmaValues:
+        for factor in factorValues:
+            if factor == "norm":
+                factor = 1 / (2.0*math.pi*sigma*sigma);
+                normStr = "_normalized";
+                kern = gkern(dim, sigma, factor).astype('float')
+            else:
+                normStr = "_factor_" + str(factor);
+                kern = gkern(dim, sigma, factor).astype('uint16')
+
+            oddEvenStr = '_even_' if dim % 2 == 0 else '_odd_'
+            tiff = TIFF.open('gaussian_2d_sigma' + str(sigma) + normStr + oddEvenStr + str(dim) + 'x' + str(dim) + '.tiff', mode='w')
+            tiff.write_image(kern)
+            tiff.close()
