6ca42eedd7713eba16c6dcadc09e60ab4e74ce5e
Carsten Schmitt <carsten.schmitt@lost-infinity.com>
2022-05-25 21:24:05 +0200

    Added CImg tiff tests.

--- source/focus_finder/tests/starmath-tests/image_tests.cpp
+++ source/focus_finder/tests/starmath-tests/image_tests.cpp
@@ -23,16 +23,28 @@
  ****************************************************************************/
 
 #include <boost/test/unit_test.hpp>
+#include <boost/test/tools/floating_point_comparison.hpp>
 
 #include "../../common/include/image.h"
 
-// TODO: Load float TIFF image and check if certain pixels have the correct value (0.1324, ....)
-// TODO: Load a float TIFF image and check if certain pixels have the correct value... (65535...)
-// TODO: Load FITS image and check that really a 16 bit image was loaded...
-
-BOOST_AUTO_TEST_CASE(tiff_float_image_test)
+/**
+ * This image is expected to have 16 bit, each pixel is supposed to have
+ * the pixel value 65535.
+ */
+BOOST_AUTO_TEST_CASE(tiff_16_bit_image_test)
 {
-        ImageT tiffFloatImage("test_data/test_image_2.tif");
-        // TODO: Test loaded pixel value...
-        //BOOST_CHECK_EQUAL(std::isnan(HfdT::calculate(darkImage, outerDiameter)), true);
+        const float expectedPixelValue = 65535.0F;
+        ImageT tiffFloatImage("test_data/test_image_7.tif");
+        BOOST_CHECK_CLOSE(tiffFloatImage(0,0), expectedPixelValue, 0.001F);
+}
+
+/**
+ * This image is expected to have 32 bit, each pixel is supposed to have
+ * the pixel value 0.5.
+ */
+BOOST_AUTO_TEST_CASE(tiff_32_bit_float_image_test)
+{
+    const float expectedPixelValue = 0.5F;
+    ImageT tiffFloatImage("test_data/test_image_20.tif");
+    BOOST_CHECK_CLOSE(tiffFloatImage(0,0), expectedPixelValue, 0.001F);
 }
