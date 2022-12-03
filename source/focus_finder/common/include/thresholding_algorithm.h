9b5579374279d2325b09317e8805cbbe66f212b3
Carsten Schmitt <carsten.schmitt@lost-infinity.com>
2022-12-03 22:42:40 +0100

    Adding [[nodiscard]] and using "default" keyword to define empty destructor.

--- source/focus_finder/common/include/thresholding_algorithm.h
+++ source/focus_finder/common/include/thresholding_algorithm.h
@@ -31,11 +31,11 @@
 
 class ThresholdingAlgorithmT {
 public:
-    virtual ~ThresholdingAlgorithmT() {};
+    virtual ~ThresholdingAlgorithmT() = default;
 
-    virtual std::string getName() const = 0;
+    [[nodiscard]] virtual std::string getName() const = 0;
 
-    virtual float calc(const ImageT &inImg, long bitDepth) const = 0;
+    [[nodiscard]] virtual float calc(const ImageT &inImg, long bitDepth) const = 0;
 };
 
 #endif /*SOURCE_FOCUS_FINDER_COMMON_INCLUDE_THRESHOLDING_ALGORITHM_H_*/
