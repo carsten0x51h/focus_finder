f594229083dc35a25bbee85b00a7808816503506
Carsten Schmitt <carsten.schmitt@lost-infinity.com>
2022-12-05 22:40:22 +0100

    Compacted and added Intensity Weighted Center (IWC) unit tests.

--- source/focus_finder/tests/starmath-tests/centroid_tests.cpp
+++ source/focus_finder/tests/starmath-tests/centroid_tests.cpp
@@ -36,12 +36,15 @@
  */
 BOOST_AUTO_TEST_SUITE(centroid_tests)
 
-
 /**
- * Calculate centroid of a black square image using the intensity weighted
- * centroiding method.
+ * Calculate centroids using the intensity weighted center (IWC)
+ * method. This array contains pairs of filename and expected centroid.
+ * The expected values were cross-checked manually.
  *
- * The IWC value for a plain image can be easily calculated analytically.
+ *
+ * For a black square image (e.g. test_image_all_pixels_value_1_5x5.tif),
+ * the IWC value for a plain image can be easily calculated:
+ *
  * Assuming a plain image where all pixel values are A, and the image
  * dimensions are M x M. Then, I2x = sum_ij(I_ij^2*x), I2y = sum_ij(I_ij^2*y)
  * and Ixy2 = sum_ij(I_ij^2) can be calculated as:
@@ -72,56 +75,41 @@ BOOST_AUTO_TEST_SUITE(centroid_tests)
  * For the 5x5 image (image index starting with 0,0), this is
  * exactly in the center.
  */
-BOOST_AUTO_TEST_CASE(intensity_weighted_centroid_all_pixel_values_equal_1_test)
+BOOST_AUTO_TEST_CASE(intensity_weighted_centroid_test)
 {
-    ImageT plainImage("test_data/test_image_22.tif"); // All pixels have the value 1 - 5x5
-
-    auto centroidAlgorithm = CentroidAlgorithmFactoryT::getInstance(CentroidAlgorithmTypeT::IWC);
-
-    std::optional<PointT<float>> centroidOpt = centroidAlgorithm->calc(plainImage);
-
-    BOOST_CHECK(centroidOpt.has_value());
-    BOOST_CHECK_EQUAL(centroidOpt.value(), PointT<float>(2.0F,2.0F));
-}
-
-/**
- * Calculate centroid of an image with one synthetic, perfect star using the intensity weighted
- * centroiding method.
- *
- * The expected centroid index position is (x,y)=(22,14).
- */
-BOOST_AUTO_TEST_CASE(intensity_weighted_centroid_perfect_star_test)
-{
-    ImageT perfectStarImage("test_data/test_image_21.tif"); // Ideal star with centroid at (x,y)=(22, 14)
-
-    auto centroidAlgorithm = CentroidAlgorithmFactoryT::getInstance(CentroidAlgorithmTypeT::IWC);
-
-    std::optional<PointT<float>> centroidOpt = centroidAlgorithm->calc(perfectStarImage);
-
-    BOOST_CHECK(centroidOpt.has_value());
-    BOOST_CHECK_EQUAL(centroidOpt.value(), PointT<float>(22.0F,14.0F));
-}
-
-/**
- * Calculate centroid of a 5x5 pixel image with all pixel values equal to 1
- * using the center of gravity method.
- *
- * The expected centroid in image coordinates (starts with (0, 0)) is
- * (xs, ys) = (2,2). A similar calculation of the expected value is shown in
- * one of the unit tests for the "Intensity Weighted Algorithm" above.
- *
- * TODO / IDEA: Better write one unit test for COG which will then be executed for
- *              different images with different expectations?
- *              -> Then, where does documentation - especially for describing the
- *              expected values - go?
- */
-BOOST_AUTO_TEST_CASE(center_of_gravity_centroid_all_pixel_values_equal_1_test)
-{
-    std::array<std::pair<std::string, PointT<float> >, 4> centroidData = {
+    std::array<std::pair<std::string, PointT<float> >, 5> centroidData = {
             std::make_pair("test_data/centroiding/test_image_all_pixels_value_1_5x5.tif", PointT<float>(2.0F,2.0F)),
             std::make_pair("test_data/centroiding/test_image_one_pixel_bottom_right_5x5.tif", PointT<float>(3.0F,3.0F)),
             std::make_pair("test_data/centroiding/test_image_one_pixel_top_left_10x10.tif", PointT<float>(1.0F,1.0F)),
-            std::make_pair("test_data/centroiding/test_image_two_opposite_pixels_5x5.tif", PointT<float>(2.0F,2.0F))
+            std::make_pair("test_data/centroiding/test_image_two_opposite_pixels_5x5.tif", PointT<float>(2.0F,2.0F)),
+            std::make_pair("test_data/centroiding/test_image_ideal_star_73x65.tif", PointT<float>(22.0F,14.0F))
+    };
+
+    auto centroidAlgorithm = CentroidAlgorithmFactoryT::getInstance(CentroidAlgorithmTypeT::IWC);
+
+    for (auto const & centroidTestRecord : centroidData) {
+        std::string imageFilename = centroidTestRecord.first;
+        ImageT testImage(imageFilename.c_str());
+        PointT<float> expectedCentroidPoint = centroidTestRecord.second;
+        std::optional<PointT<float>> centroidOpt = centroidAlgorithm->calc(testImage);
+
+        BOOST_CHECK(centroidOpt.has_value());
+        BOOST_CHECK_EQUAL(centroidOpt.value(), expectedCentroidPoint);
+    }
+}
+
+/**
+ * Calculate centroids using the center of gravity (COG) method.
+ * This array contains pairs of filename and expected centroid.
+ * The expected values were cross-checked manually.
+ */
+BOOST_AUTO_TEST_CASE(center_of_gravity_centroid_test)
+{
+    std::array<std::pair<std::string, PointT<float> >, 4> centroidData = {
+        std::make_pair("test_data/centroiding/test_image_all_pixels_value_1_5x5.tif", PointT<float>(2.0F,2.0F)),
+        std::make_pair("test_data/centroiding/test_image_one_pixel_bottom_right_5x5.tif", PointT<float>(3.0F,3.0F)),
+        std::make_pair("test_data/centroiding/test_image_one_pixel_top_left_10x10.tif", PointT<float>(1.0F,1.0F)),
+        std::make_pair("test_data/centroiding/test_image_two_opposite_pixels_5x5.tif", PointT<float>(2.0F,2.0F))
     };
 
     auto centroidAlgorithm = CentroidAlgorithmFactoryT::getInstance(CentroidAlgorithmTypeT::COG);
@@ -137,7 +125,7 @@ BOOST_AUTO_TEST_CASE(center_of_gravity_centroid_all_pixel_values_equal_1_test)
     }
 }
 
-// TODO: Implement a few more COG tests.
+// TODO: Need one test image which gives different results for IWC and COG!
 // TODO: Remove old Centroid from code
 
 
