7e96af2f3d91ad7db745f5e23eb1606e6cc9ee8a
Carsten Schmitt <carsten.schmitt@lost-infinity.com>
2022-12-16 00:15:01 +0100

    Replaced subtract_background adapter by simple method.

--- source/focus_finder/common/include/pipeline/adapter/subtract_background.h
+++ source/focus_finder/common/include/pipeline/adapter/subtract_background.h
@@ -34,98 +34,33 @@
 
 namespace AstroImagePipeline {
 
+    // In:  range<ImageT>
+    // Out: range<ImageT>
+    auto
+    subtract_background(const std::shared_ptr<ThresholdingAlgorithmT> & thresholding_algorithm) {
+        return boost::adaptors::transformed(
+                [=](const std::shared_ptr<ImageT> &image) {
+                    const ImageT & inputImageRef = *image;
 
-    template<typename Value>
-    class subtract_background_value {
-    public:
-        typedef const typename ThresholdingAlgorithmTypeT::TypeE argument_type;
-        typedef std::shared_ptr<ImageT> &result_type;
+                    DEBUG_IMAGE_DISPLAY(inputImageRef, "subtract_background_in", FOFI_SUBTRACT_BACKGROUND_DEBUG);
 
-        explicit subtract_background_value(const argument_type & threshold_type) {
-            m_thresholding_algorithm = ThresholdingAlgorithmFactoryT::getInstance(threshold_type);
-        }
+                    // TODO: Handle bit depth... do not hardcode here...
+                    float threshold = thresholding_algorithm->calc(inputImageRef, 16);
 
-        // TODO: Then do not return a copy! -> at least return shared_ptr<ImageT> !
-        std::shared_ptr<ImageT> operator()(const Value & image) const {
+                    auto subImage = std::make_shared<ImageT>(inputImageRef, "xy");
 
-            const ImageT & inputImageRef = *image;
+                    ImageT & subImageRef = (*subImage);
 
-            DEBUG_IMAGE_DISPLAY(inputImageRef, "subtract_background_in", FOFI_SUBTRACT_BACKGROUND_DEBUG);
+                    cimg_forXY(inputImageRef, x, y) {
+                            subImageRef(x, y) = (inputImageRef(x, y) < threshold ? 0 : inputImageRef(x, y) - threshold);
+                        }
 
-            // TODO: Handle bit depth... do not hardcode here...
-            float threshold = m_thresholding_algorithm->calc(inputImageRef, 16);
+                    DEBUG_IMAGE_DISPLAY(subImageRef, "subtract_background_out", FOFI_SUBTRACT_BACKGROUND_DEBUG);
 
-            auto subImage = std::make_shared<ImageT>(inputImageRef, "xy");
-
-            ImageT & subImageRef = (*subImage);
-
-            cimg_forXY(inputImageRef, x, y) {
-                subImageRef(x, y) = (inputImageRef(x, y) < threshold ? 0 : inputImageRef(x, y) - threshold);
-            }
-
-            DEBUG_IMAGE_DISPLAY(subImageRef, "subtract_background_out", FOFI_SUBTRACT_BACKGROUND_DEBUG);
-
-            return subImage;
-        }
-
-    private:
-        std::shared_ptr<ThresholdingAlgorithmT> m_thresholding_algorithm;
-    };
-
-    template<typename Range>
-    class subtract_background_range
-            : public boost::iterator_range<
-                    boost::transform_iterator<
-                    subtract_background_value<typename boost::range_value<Range>::type>,
-                    typename boost::range_iterator<Range>::type> > {
-    private:
-    typedef typename boost::range_value<Range>::type value_type;
-    typedef typename boost::range_iterator<Range>::type iterator_base;
-    typedef subtract_background_value<value_type> Fn;
-    typedef boost::transform_iterator<Fn, iterator_base> subtract_background_iterator;
-    typedef boost::iterator_range<subtract_background_iterator> base_t;
-
-    public:
-        //subtract_background_range(Range &rng, value_type from, value_type to)
-    subtract_background_range(Range &rng, typename ThresholdingAlgorithmTypeT::TypeE from)
-            : base_t(subtract_background_iterator(boost::begin(rng), Fn(from)),
-                     subtract_background_iterator(boost::end(rng), Fn(from))) {
+                    return subImage;
+                }
+        );
     }
-};
-
-
-template<typename T>
-class subtract_background_holder : public boost::range_detail::holder<T> {
-public:
-    explicit subtract_background_holder(const T &threshold_type)
-            : boost::range_detail::holder<T>(threshold_type) {}
-
-private:
-    void operator=(const subtract_background_holder &) = delete;
-};
-
-
-static boost::range_detail::forwarder<subtract_background_holder>
-        subtract_background = boost::range_detail::forwarder<subtract_background_holder>();
-
-
-template<typename SinglePassRange>
-inline subtract_background_range<SinglePassRange>
-operator|(SinglePassRange &rng,
-          subtract_background_holder<typename ThresholdingAlgorithmTypeT::TypeE /*typename boost::range_value<SinglePassRange>::type>*/> &f) {
-    return subtract_background_range<SinglePassRange>(rng, f.val);
-}
-
-
-// const variant
-//
-template<typename SinglePassRange>
-inline subtract_background_range<const SinglePassRange>
-operator|(const SinglePassRange &rng,
-          const subtract_background_holder<typename ThresholdingAlgorithmTypeT::TypeE> &f) {
-
-    return subtract_background_range<const SinglePassRange>(rng, f.val);
-}
 
 } // End namespace AstroImagePipeline
 
