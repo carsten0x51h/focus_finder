5cc5ed9520eb786cd63dc7b9a51c134f7131a699
Carsten Schmitt <carsten.schmitt@lost-infinity.com>
2022-12-19 20:09:18 +0100

    Implemented initial version of pipeline multiply_by() and unit tests.

---
+++ source/focus_finder/common/include/pipeline/view/multiply_by.h
@@ -0,0 +1,88 @@
+/*****************************************************************************
+ *
+ *  FoFi - A free, automatic telescope focus finder.
+ *
+ *  Copyright(C) 2019 Carsten Schmitt <c [at] lost-infinity.com>
+ *
+ *  More info on https://www.lost-infinity.com
+ *
+ *  This program is free software ; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation ; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY ; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program ; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ ****************************************************************************/
+
+#ifndef FOFI_MULTIPLY_BY_H
+#define FOFI_MULTIPLY_BY_H
+
+#include <range/v3/view/transform.hpp>
+
+#include "../../image.h"
+
+#define FOFI_MULTIPLY_BY_DEBUG 0
+
+namespace AstroImagePipeline {
+
+    /**
+     *
+     * @tparam ImageType
+     * @param imageToMultiplyByPtr
+     * @return
+     *
+     * TODO: Check that all images have the same dimension? Could be configured by parameter...
+     */
+    template<typename ImageType=float>
+    auto
+    multiply_by(const std::shared_ptr<ImageT> & imageToMultiplyByPtr) {
+        return ranges::views::transform(
+                [=](const std::shared_ptr<cimg_library::CImg<ImageType> > & image) {
+                    const cimg_library::CImg<ImageType> & inputImageRef = *image;
+
+                    DEBUG_IMAGE_DISPLAY(inputImageRef, "multiply_by_image_in", FOFI_MULTIPLY_BY_DEBUG);
+
+                    auto result_image = std::make_shared<cimg_library::CImg<ImageType>>(inputImageRef.get_mul(*imageToMultiplyByPtr));
+
+                    DEBUG_IMAGE_DISPLAY(*result_image, "multiply_by_image_out", FOFI_MULTIPLY_BY_DEBUG);
+
+                    return result_image;
+                }
+        );
+    }
+
+
+    /**
+     *
+     * @tparam ImageType
+     * @param scalarToMultiplyBy
+     * @return
+     */
+    template<typename ImageType=float>
+    auto
+    multiply_by(ImageType scalarToMultiplyBy) {
+        return ranges::views::transform(
+                [=](const std::shared_ptr<cimg_library::CImg<ImageType> > & image) {
+                    const cimg_library::CImg<ImageType> & inputImageRef = *image;
+
+                    DEBUG_IMAGE_DISPLAY(inputImageRef, "multiply_by_scalar_in", FOFI_MULTIPLY_BY_DEBUG);
+
+                    auto result_image = std::make_shared<cimg_library::CImg<ImageType>>(inputImageRef * scalarToMultiplyBy);
+
+                    DEBUG_IMAGE_DISPLAY(*result_image, "multiply_by_out", FOFI_MULTIPLY_BY_DEBUG);
+
+                    return result_image;
+                }
+        );
+    }
+} // End namespace AstroImagePipeline
+
+#endif //FOFI_MULTIPLY_BY_H
---
+++ source/focus_finder/tests/starmath-tests/pipeline_multiply_by_tests.cpp
@@ -0,0 +1,85 @@
+/*****************************************************************************
+ *
+ *  FoFi - A free, automatic telescope focus finer.
+ *
+ *  Copyright(C) 2019 Carsten Schmitt <c [at] lost-infinity.com>
+ *
+ *  More info on https://www.lost-infinity.com
+ *
+ *  This program is free software ; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation ; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY ; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program ; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ ****************************************************************************/
+
+#include <range/v3/range/conversion.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+#include "../../common/include/pipeline/view/images.h"
+#include "../../common/include/pipeline/view/multiply_by.h"
+
+BOOST_AUTO_TEST_SUITE(pipeline_multiply_by_tests)
+
+using namespace AstroImagePipeline;
+using namespace ranges;
+
+/**
+ *
+ */
+BOOST_AUTO_TEST_CASE(pipeline_multiply_by_image_test)
+{
+    ImageT expectedResultImage(5,5,1,1,3); // 5x5 - bg value 3
+    expectedResultImage(2,2) = 200.0F;
+
+    auto imageToMultiplyByPtr = std::make_shared<ImageT>("test_data/image_processing_pipeline/multiply_by/test_image_all_1_center_2_5x5.tiff");
+
+    const std::vector<std::string> imageFilenames {
+            "test_data/image_processing_pipeline/multiply_by/test_image_all_3_center_100_5x5.tiff",
+    };
+
+    auto resultImagesPtr = imageFilenames
+                           | images()
+                           | multiply_by(imageToMultiplyByPtr)
+                           | to<std::vector>();
+
+    // NOTE: Exactly one image is expected
+    const ImageT & resultImage = *(resultImagesPtr.at(0));
+
+    BOOST_TEST(resultImage == expectedResultImage);
+}
+
+/**
+ *
+ */
+BOOST_AUTO_TEST_CASE(pipeline_multiply_by_scalar_test)
+{
+    ImageT expectedResultImage(5,5,1,1,9); // 5x5 - bg value 9
+    expectedResultImage(2,2) = 300.0F;
+
+    const std::vector<std::string> imageFilenames {
+            "test_data/image_processing_pipeline/multiply_by/test_image_all_3_center_100_5x5.tiff",
+    };
+
+    auto resultImagesPtr = imageFilenames
+                           | images()
+                           | multiply_by(3.0F)
+                           | to<std::vector>();
+
+    // NOTE: Exactly one image is expected
+    const ImageT & resultImage = *(resultImagesPtr.at(0));
+
+    BOOST_TEST(resultImage == expectedResultImage);
+}
+
+BOOST_AUTO_TEST_SUITE_END();
